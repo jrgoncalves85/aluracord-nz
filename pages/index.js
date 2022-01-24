6c207d53fbcd69fb339844ed0aaf86640b6f5777
Júnior Gonçalves <jrgoncalves85@gmail.com>
2022-01-24 13:47:36 -0300

    funções no react

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,41 @@
+function GlobalStyle() {
+    return (
+        <style global jsx >{`
+            *{
+                margin:0;
+                padding:0;
+                background-color:silver;
+            }
+            body{
+                font-family: sans-serif;
+            }
+        `}</style>
+    );
+}
+
+function Titulo(props) {
+    console.log(props);
+    const Tag = props.tag;
+    return (
+        <>
+            <Tag>{props.children}</Tag>
+            <style jsx >{`
+                ${Tag} {
+                    color: red;
+                    font-size: 24px;
+                }
+            `}</style> 
+        </>
+    );
+
+}
+
 function HomePage() {
     return (
         <div>
-            <h1>Aluracord - New Zealand</h1>
-            <h2>Seja Bem-Vindo!!</h2>
-            <style jsx >{`
-                h1{
-                    color: red;
-                }
-            `}</style>  
+            <GlobalStyle />
+            <Titulo tag="h2">Aluracord - New Zealand</Titulo>
+            <p>Seja Bem-Vindo!!</p>             
         </div>
         
     )
